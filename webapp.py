843cd86b06b2637767e5fd65989a17daee17bd67
Derek Daniels <dbd@burrow>
2019-03-05 22:56:49 -0500

    Add hostname to example

--- webapp.py
+++ webapp.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
+import socket
 
 @app.route("/")
 def hello():
-    return "Hello world from kubernetes"
+    hostname = socket.gethostname()
+    return "Hello world from {hostname}".format(hostname=hostname)
  
 if __name__ == "__main__":
     app.run()
